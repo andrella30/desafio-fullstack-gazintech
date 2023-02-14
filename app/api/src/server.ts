c0c6842662af1bc785e32556c36557feb34112da
Gabriel Andrella <gabriel.andrella30@gmail.com>
2023-02-14 08:56:32 -0300

    Adicionando cors

--- app/api/src/server.ts
+++ app/api/src/server.ts
@@ -1,14 +1,16 @@
 import express from 'express'
-
+import cors from 'cors'
 import "./database"
 import desenvolvedorRouter from './modules/desenvolvedores/routes/desenvolvedor.routes'
 import niveisRouter from './modules/niveis/routes/niveis.routes'
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
+
 app.use("/niveis", niveisRouter)
-app.use("/desenvolvedor", desenvolvedorRouter)
+app.use("/desenvolvedores", desenvolvedorRouter)
 
 app.listen(3333, () => console.log("Server is running")) 
